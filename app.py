39a86b90c672f0213ab460036ffc5b31a038a696
ViktorSinagl <viktor.sinagl@gmail.com>
2024-10-14 17:30:15 +0200

    jedeme bomby

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, Blueprint
+from companies import InsuranceCompany
+import yaml
+
+
+
+def create_companies():
+	# here but database retrieval code
+	data = None
+	companies = []
+	with open('templates/companies/companies.yaml') as file:
+		data = yaml.safe_load(file)
+	for elem in data:
+		company = InsuranceCompany(elem)
+		companies.append(company)
+	return companies
+	
+	
 
 
 views = Blueprint("views", "home")
+companies = create_companies()
+# HARD coded active insurance company
+active_insurance_company = companies[0]
+print("THE PRICE IS: ",companies[0].price)
 
 @views.route("/")
 def home():
@@ -9,8 +30,8 @@ def home():
 
 @views.route("/insurance")
 def insurance():
-      return render_template("insurance.html")
-
+	return render_template("insurance.html", user_insurance = active_insurance_company,
+						companies = companies)
 
 
 # app itself
---
+++ companies.py
@@ -0,0 +1,10 @@
+
+class InsuranceCompany:
+
+	def __init__(self, data:dict):
+		self.title = data['title']
+		self.description = data['description']
+		self.logo = data['logo']
+		self.webpage = data['webpage']
+		self.price = data['price']
+	
